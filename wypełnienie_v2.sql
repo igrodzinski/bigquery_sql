d5c6062b178e6b2ddcb9440f1f3f60eedb11c54e
igrodzinski <31898177+igrodzinski@users.noreply.github.com>
2025-07-14 15:03:42 +0200

    Update wypełnienie_v2.2.sql

--- wypełnienie_v2.sql
+++ wypełnienie_v2.sql
@@ -1,20 +1,20 @@
+DECLARE var_dataset_name STRING DEFAULT 'DM_';
+DECLARE var_table_name STRING DEFAULT 'DIM_BUSINESS';
+DECLARE sql_string STRING;
+--
 CREATE TEMP TABLE columns_list AS
 SELECT column_name
-FROM `my_dataset.INFORMATION_SCHEMA.COLUMNS`
-WHERE table_name = 'my_table';
-
-DECLARE sql_string STRING;
-
+FROM `DM_CLIENT_1610.INFORMATION_SCHEMA.COLUMNS`
+WHERE table_name = var_table_name;
+ 
 SET sql_string = (
   SELECT STRING_AGG(
-    CONCAT(
-      "SELECT '", column_name, "' AS column_name, ",
-      "SAFE_DIVIDE(COUNT(`", column_name, "`), COUNT(*)) * 100 AS fill_percent ",
-      "FROM `my_dataset.my_table`"
+    CONCAT("SELECT '", column_name, "' AS column_name, ",
+    "SAFE_DIVIDE(COUNT(`", column_name, "`), COUNT(*)) * 100 AS fill_percet ",
+    "FROM `", var_dataset_name, ".", var_table_name, "`"  
     ),
-    " UNION ALL "
-  )
+    " UNION ALL ")
   FROM columns_list
 );
-
+ 
 EXECUTE IMMEDIATE sql_string;
