74badd694a19b6735e2e9138598a03e0111d1161
igrodzinski <31898177+igrodzinski@users.noreply.github.com>
2025-07-14 14:52:33 +0200

    Update wypełnienie_v2.1.sql

--- wypełnienie_v2.sql
+++ wypełnienie_v2.sql
@@ -1,26 +1,19 @@
--- ZAMIEN TO NA WLASNE
-DECLARE dataset_name STRING DEFAULT 'my_dataset';
-DECLARE table_name STRING DEFAULT 'my_table';
-
--- Krok 1: Pobierz listę kolumn
 CREATE TEMP TABLE columns_list AS
 SELECT column_name
 FROM `my_dataset.INFORMATION_SCHEMA.COLUMNS`
-WHERE table_name = table_name;
+WHERE table_name = 'my_table';
 
--- Krok 2: Zbuduj i wykonaj zapytanie dynamiczne
 DECLARE sql_string STRING;
 
 SET sql_string = (
   SELECT STRING_AGG(
-    FORMAT("""
-    SELECT 
-      '%1$s' AS column_name,
-      SAFE_DIVIDE(COUNT(%1$s), COUNT(*)) * 100 AS fill_percent
-    FROM `%2$s.%3$s`""",
-      column_name, dataset_name, table_name
-    )
-    , ' UNION ALL ')
+    CONCAT(
+      "SELECT '", column_name, "' AS column_name, ",
+      "SAFE_DIVIDE(COUNT(`", column_name, "`), COUNT(*)) * 100 AS fill_percent ",
+      "FROM `my_dataset.my_table`"
+    ),
+    " UNION ALL "
+  )
   FROM columns_list
 );
 
